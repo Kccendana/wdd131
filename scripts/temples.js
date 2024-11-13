73e417992589b04359c384b9d32890c1e753f18c
kcccendana <kc_telyn@yahoo.com>
2024-11-14 00:14:48 +0900

    modified the css and script

--- scripts/temples.js
+++ scripts/temples.js
@@ -7,28 +7,16 @@ lastModified.textContent = document.lastModified;
 // nav button toggle
 const navButton = document.querySelector('.nav-button');
 const navList = document.querySelector('.navi');
+const logo = document.querySelector('.logo');
 
 navButton.addEventListener('click', () => {
   navButton.classList.toggle('close');
-  navList.classList.toggle('show')
+  navList.classList.toggle('show');
+  logo.classList.toggle('hidden');
 }, false);
 
-window.onresize = () => { if (window.innerWidth > 760) navList.classList.remove('show'); navButton.classList.remove('close');};
-// const navButton = document.querySelector('.nav-button')
-// const navList = document.querySelector('.navi');
-// const navMenu = document.querySelectorAll('.navMenu');
-// const logo = document.querySelector('.logo');
-
-// navButton.addEventListener('click', () => {
-//   navList.classList.toggle('show');
-//   navButton.classList.toggle('close');
-//   logo.classList.toggle('hide');
-// }, false);
-
-// navMenu.forEach((links) => links.addEventListener('click', () => {
-//   navList.classList.remove('show');
-//   navButton.classList.remove('close');
-//   logo.classList.remove('hide');
-// }));
-
-// window.onresize = () => { if (window.innerWidth > 760) navList.classList.remove('show'); };
+window.onresize = () => { if (window.innerWidth > 760) 
+  navList.classList.remove('show'); 
+  navButton.classList.remove('close');
+  logo.classList.remove('hidden');
+}
